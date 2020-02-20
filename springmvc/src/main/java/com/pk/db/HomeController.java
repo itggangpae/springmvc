88dbc109c5b09705f6712d7ee44ec89ad7039a3e
admin <admin@DESKTOP-40100>
2020-02-20 16:20:43 +0900

    0220

--- springmvc/src/main/java/com/pk/db/HomeController.java
+++ springmvc/src/main/java/com/pk/db/HomeController.java
@@ -2,38 +2,42 @@ package com.pk.db;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+import com.pk.db.domain.Item;
+import com.pk.db.service.ItemService;
+
+
 @Controller
 public class HomeController {
+	//Service 객체를 주입
+	@Autowired
+	private ItemService itemService;
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	public String home(HttpServletRequest request, Model model) {;
+		//서비스 메소드 호출
+		List<Item> list = itemService.allItem(request);
+		//데이터 저장
+		model.addAttribute("list", list);
+	
 		return "home";
 	}
-	
 }
+
+
+
+
+
+
--- springmvc/src/main/webapp/WEB-INF/views/home.jsp
+++ springmvc/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,34 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!-- if 나 forEach를 사용하기 위한 태그 라이브러리를 설정 -->
+<%@ taglib prefix="c"
+uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
 <html>
 <head>
-	<title>Home</title>
+<meta charset="UTF-8">
+<title>목록 출력</title>
+<link rel="stylesheet" href="./css/item.css">
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
-
-<P>  The time on the server is ${serverTime}. </P>
+	<h2 align='center'>상품 목록</h2>
+	<table border='1' align='center'>
+		<tr class='header'>
+			<th width='80'>상품ID</th>
+			<th width='160'>상품이름</th>
+			<th width='80'>가격</th>
+		</tr>
+		<c:forEach items="${list}" var="item">
+			<tr class="record">
+				<td align='center'>${item.itemid}</td>
+				<td>&nbsp; &nbsp; ${item.itemname}</td>
+				<td align='right'>${item.price}원&nbsp; &nbsp;</td>
+			</tr>
+		</c:forEach>	
+	</table>
 </body>
 </html>
+
+
+
+
