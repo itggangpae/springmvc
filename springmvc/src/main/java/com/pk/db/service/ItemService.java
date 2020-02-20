102c435e82d3945ed7fd744179116a8ff9e255d7
admin <admin@DESKTOP-40100>
2020-02-20 15:08:45 +0900

    0220

---
+++ springmvc/src/main/java/com/pk/db/service/ItemService.java
@@ -0,0 +1,12 @@
+package com.pk.db.service;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.pk.db.domain.Item;
+
+public interface ItemService {
+	//전체 데이터를 가져오는 메소드
+	public List<Item> allItem(HttpServletRequest request);
+}
---
+++ springmvc/src/main/java/com/pk/db/service/ItemServiceImpl.java
@@ -0,0 +1,26 @@
+package com.pk.db.service;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pk.db.dao.ItemDao;
+import com.pk.db.domain.Item;
+
+//Bean을 자동생성해주는 어노테이션
+@Service
+public class ItemServiceImpl implements ItemService {
+	//ItemDao 타입의 Bean이 있으면 자동으로 주입시켜주는 어노테이션
+	@Autowired
+	private ItemDao itemDao;
+
+	@Override
+	public List<Item> allItem(HttpServletRequest request) {
+		//Dao의 메소드에 파라미터가 없는 경우는 Dao 메소드를 호출해서 리턴
+		return itemDao.allItem();
+	}
+
+}
